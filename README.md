3557fa3f1581883a589d17bef4ca88847ac1c83f
Marco Rebhan <git@marco-rebhan.de>
2015-07-29 19:34:08 +0200

    Looks done to me.

---
+++ README.md
@@ -0,0 +1,25 @@
+cursors.io-imgdata
+==================
+
+A image generator for the cursors.io webgame. Uses the "Cursor Jesus" hack.
+
+Instructions:
+
+Run the program using `java -jar imgdata.jar <-v> <-h> <-iINFILE.PNG> <-oOUTFILE.TXT>`.
+The program creates the modded cursor jesus file.
+Press ctrl+shift+j in your browser and copy/paste the entire script into the command prompt console window.
+Hit enter.
+Cursors.io should now reload and enjoy your mod!
+Press + to draw image. There are more options located at the top of the output file.
+
+
+Options:
+
+-v: vertical bars
+
+-h: horizontal bars
+
+-i: Input file (Best: PNG format, black and white, 256x256-ish size)
+
+-o: Output file
+
--- src/com/github/mrebhan/imgdatacreator/Start.java
+++ src/com/github/mrebhan/imgdatacreator/Start.java
@@ -1,15 +1,24 @@
 package com.github.mrebhan.imgdatacreator;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+@SuppressWarnings("deprecation")
 public class Start {
 
 	public static void main(String[] args) {
@@ -25,12 +34,12 @@ public class Start {
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		
+
 		boolean hor = false;
 		boolean ver = false;
 		File in = null;
 		File out = null;
-		
+
 		if (!line.hasOption('h') && !line.hasOption('v')) {
 			System.err.println("Needs at least one of -h and -v");
 			System.exit(-1);
@@ -44,17 +53,92 @@ public class Start {
 			System.exit(-1);
 		}
 
-		
+
 		if (line.hasOption('h'))
 			hor = true;
 		if (line.hasOption('v'))
 			ver = true;
 		in = new File(line.getOptionValue('i'));
 		out = new File(line.getOptionValue('o'));
-		
-		
-		
-		
+
+		if (!in.isFile()) {
+			System.err.println("Input file doesn't exist");
+			System.exit(-1);
+		}
+
+		try {
+			BufferedImage img = ImageIO.read(in);
+			ArrayList<String> data = new ArrayList<String>();
+			String dataStr = "";
+			if (hor) {
+				int lX = -1;
+				int mX = -1;
+				for (int y = 0; y < img.getHeight(); y++) {
+					for (int x = 0; x < img.getWidth(); x++) {
+						int r = img.getColorModel().getRed(img.getRaster().getDataElements(x, y, null));
+						int g = img.getColorModel().getGreen(img.getRaster().getDataElements(x, y, null));
+						int b = img.getColorModel().getBlue(img.getRaster().getDataElements(x, y, null));
+
+						int comp = r+g+b/3;
+						int bn = 128;
+
+						if (comp < bn) {
+							if (lX == -1)
+								lX = x;
+							mX = x;
+						} else {
+							if (lX != -1 && mX != -1) {
+								dataStr += "[" + y + ", " + lX + ", " + y + ", " + mX + "],\n";
+							}
+							lX = -1;
+							mX = -1;
+						}
+					}
+					lX = -1;
+					mX = -1;
+				}
+			}
+			if (ver) {
+				int lY = -1;
+				int mY = -1;
+				for (int x = 0; x < img.getWidth(); x++) {
+					for (int y = 0; y < img.getHeight(); y++) {
+						int r = img.getColorModel().getRed(img.getRaster().getDataElements(x, y, null));
+						int g = img.getColorModel().getGreen(img.getRaster().getDataElements(x, y, null));
+						int b = img.getColorModel().getBlue(img.getRaster().getDataElements(x, y, null));
+
+						int comp = r+g+b/3;
+						int bn = 128;
+
+						if (comp < bn) {
+							if (lY == -1)
+								lY = y;
+							mY = y;
+						} else {
+							if (lY != -1 && mY != -1) {
+								dataStr += "[" + lY + ", " + x + ", " + mY + ", " + x + "],\n";
+							}
+							lY = -1;
+							mY = -1;
+						}
+					}
+					lY = -1;
+					mY = -1;
+				}
+			}
+			InputStream str = Start.class.getClassLoader().getResourceAsStream("cursor_jesus.txt");
+		    BufferedReader r = new BufferedReader(new InputStreamReader(str));
+		    String s = "";
+		    String buf = "";
+		    while ((buf = r.readLine()) != null) {
+		    	s += buf + '\n';
+		    }
+		    s = s.replace("<REPLACE_HERE>", dataStr);
+		    Files.write(out.toPath(), s.getBytes(), StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
